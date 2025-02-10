f9ab7cd468e7cdbecaacd6164359a094fae432df
John Pope <jp+mailopoly@bellgeorge.com>
2025-02-11 06:36:47 +1100

    ok

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ docker run --gpus all \
   python inference.py \
     --data_root "./checkpoints/ditto_trt_Ampere_Plus" \
     --cfg_pkl "./checkpoints/ditto_cfg/v0.4_hubert_cfg_trt.pkl" \
-    --audio_path "/app/input/your_audio.wav" \
-    --source_path "/app/input/your_image.png" \
+    --audio_path "./example/audio.wav" \
+    --source_path "./example/image.png" \
     --output_path "/app/output/result.mp4"
